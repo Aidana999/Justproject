0a8b5847b65d56493bc60e2e705c28aa05d7cf7a
Aidana999 <49874037+Aidana999@users.noreply.github.com>
2019-05-12 11:28:17 +0300

    commit minor changes

--- Main1/src/fifteen/Frame1.java
+++ Main1/src/fifteen/Frame1.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Frame1 extends JFrame
+public class Frame1 extends JFrame implements KeyListener
 {
  JPanel contentPane;
  JPanel gamePane = new JPanel();
@@ -269,3 +269,26 @@ public class Frame1 extends JFrame
   }
  }
 }
+
+//    /** Handle the key typed event from the text field. */
+//    public void keyTyped(KeyEvent e) {
+//        //displayInfo(e, "KEY TYPED: ");
+//        System.out.printf("key typed");
+//    }
+//
+//    /** Handle the key-pressed event from the text field. */
+//    public void keyPressed(KeyEvent e) {
+//        int keycode = e.getKeyCode();
+//        if (keycode == KeyEvent.VK_NUMPAD8)
+//        //displayInfo(e, "KEY PRESSED: ");
+//        System.out.printf("key pressed");
+//    }
+//
+//    /** Handle the key-released event from the text field. */
+//    public void keyReleased(KeyEvent e) {
+//        //displayInfo(e, "KEY RELEASED: ");
+//        System.out.printf("key released");
+//    }
+//
+//
+//}
