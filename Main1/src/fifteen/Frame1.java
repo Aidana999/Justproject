3370709204c3b8f9b6eebba56e5df027a9ce0a4e
Aidana <49874037+Aidana999@users.noreply.github.com>
2019-05-14 18:05:06 +0300

    matrix array initialized

--- Main1/src/fifteen/Frame1.java
+++ Main1/src/fifteen/Frame1.java
@@ -35,7 +35,7 @@ public class Frame1 extends JFrame {
         this.setSize(new Dimension(206, 275));
         this.setTitle("Fifteen");
 
-        this.setResizable(false);
+        
 
         JMenuBar menuBar = new JMenuBar();
 
@@ -120,6 +120,10 @@ public class Frame1 extends JFrame {
             int b = (int) (Math.random() * 4);
             int a2 = (int) (Math.random() * 4);
             int b2 = (int) (Math.random() * 4);
+
+            int c = matrix[a][b];
+            matrix[a][b] = matrix[a2][b2];
+            matrix[a2][b2] = c;
         }
     }
     
